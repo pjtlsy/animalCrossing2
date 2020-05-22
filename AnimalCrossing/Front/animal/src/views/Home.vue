62c904690c04a5bab1dca2f581cda5057ff10388
SeolYu <seolyu.90@gmail.com>
2020-05-22 11:37:00 +0900

    [Update] Home화면 - 설유
    css 수정 필요

--- AnimalCrossing/Front/animal/src/views/Home.vue
+++ AnimalCrossing/Front/animal/src/views/Home.vue
@@ -1,29 +1,24 @@
 <template>
   <div class="home">
-    <!-- <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" /> -->
     <div class="box box1">
-      <!-- <div class="logoImg">
-        <img class="logo" src="../assets/images/logo.png" alt="">
-      </div> -->
-      <div class="box1Text">
-        물고기 곤충
+      <div class="box1Con">
+        <img class="box1IconImg" src="../assets/images/logo.png" alt="">
+        <div class="box1Text">
+          물고기 곤충
+        </div>
       </div>
     </div>
+
     <div class="box box2">화석 미술품</div>
+
     <div class="box box3">꽃</div>
     <div class="box box4">게시판</div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-// import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
-  // name: "Home",
   components: {
-    // HelloWorld
   }
 };
 </script>
