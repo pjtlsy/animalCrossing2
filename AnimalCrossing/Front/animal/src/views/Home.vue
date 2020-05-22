1f108df77f81185e2f3c31e6e1da946a40edf39f
SeolYu <seolyu.90@gmail.com>
2020-05-22 09:06:01 +0900

    [Update] Home화면 - 설유

--- AnimalCrossing/Front/animal/src/views/Home.vue
+++ AnimalCrossing/Front/animal/src/views/Home.vue
@@ -2,7 +2,14 @@
   <div class="home">
     <!-- <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" /> -->
-    <div class="box box1">물고기 곤충</div>
+    <div class="box box1">
+      <!-- <div class="logoImg">
+        <img class="logo" src="../assets/images/logo.png" alt="">
+      </div> -->
+      <div class="box1Text">
+        물고기 곤충
+      </div>
+    </div>
     <div class="box box2">화석 미술품</div>
     <div class="box box3">꽃</div>
     <div class="box box4">게시판</div>
